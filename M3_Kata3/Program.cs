d76067a13db855377a04fc89bca4a864348bb411
Martin Lenart <martin_lenart@icloud.com>
2021-11-04 13:27:07 +0100

    Kata3 Added

---
+++ M3_Kata3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace M3_Kata3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IVariousDates mySpottedDates = new VariousDates(100);
+            Console.WriteLine(mySpottedDates);
+
+            Console.WriteLine($"Sorted {mySpottedDates.Count()} dates!");
+            mySpottedDates.Sort();
+            Console.WriteLine(mySpottedDates);
+
+            for (int month = 1; month <= 12; month++)
+            {
+                Console.WriteLine($"Month nr {month} constains {mySpottedDates.Count(month)} dates.");
+            }
+
+            Console.WriteLine($"\nmySpottedDates[0]: {mySpottedDates[0]}");
+
+        }
+    }
+}
---
+++ M3_Kata3/VariousDates.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M3_Kata3
+{
+ 
+
+    class VariousDates : IVariousDates
+    {
+        List<SpotDate> _itemList = new List<SpotDate>();
+        public int Count(int month)
+        {
+            int c = 0;
+            foreach (var item in _itemList)
+            {
+                if (item.date.Month == month)
+                    c++;
+            }
+            return c;
+        }
+
+        public bool Find(int month)
+        {
+            foreach (var item in _itemList)
+            {
+                if (item.date.Month == month)
+                    return true;
+            }
+            return false;
+        }
+
+        public int Count() => _itemList.Count;
+
+        public void Sort() => _itemList.Sort();
+
+        public SpotDate this[int idx] => _itemList[idx];
+
+        public override string ToString()
+        {
+            string sRet = "";
+            for (int i = 0; i < _itemList.Count; i++)
+            {
+                sRet += $"{_itemList[i]} ";
+
+                if ((i + 1) % 5 == 0)
+                    sRet += "\n";
+            }
+            return sRet;
+        }
+
+        public VariousDates(int NrOfItems)
+        {
+            var rnd = new Random();
+            for (int i = 0; i < NrOfItems; i++)
+            {
+                try
+                {
+                    SpotColor color = (SpotColor) rnd.Next((int)SpotColor.Red, (int)SpotColor.White + 1);
+                    int year = rnd.Next(2000, 2022);
+                    int month = rnd.Next(1, 13);
+                    int day = rnd.Next(1, 32);
+
+                    _itemList.Add(new SpotDate
+                    {
+                        date = new DateTime(year, month, day),
+                        spotColor = color
+                    });
+                }
+                catch 
+                { 
+                    i--; 
+                }
+            }
+        }
+    }
+}
